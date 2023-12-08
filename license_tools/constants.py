f0c55cc35955e7b8d9126d5fcd482455efb965d3
Stefan <96178532+stefan6419846@users.noreply.github.com>
2023-12-08 16:12:28 +0100

    add missing file header

--- license_tools/constants.py
+++ license_tools/constants.py
@@ -1,1 +1,8 @@
+# Copyright (c) stefan6419846. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+# See http://www.apache.org/licenses/LICENSE-2.0 for the license text.
+
+from __future__ import annotations
+
+
 NOT_REQUESTED = object()
