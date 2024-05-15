bc34c4f881cfdf890963a116cd66ae9ea9f1dd92
Stefan <96178532+stefan6419846@users.noreply.github.com>
2024-05-15 17:34:01 +0200

    improve copyright notice for monkey-patching

--- license_tools/tools/rpm_tools.py
+++ license_tools/tools/rpm_tools.py
@@ -664,7 +664,42 @@ def check_rpm_headers(path: Path) -> str | None:
     return rendered
 
 
-# Patch broken extraction of integers: https://github.com/srossross/rpmfile/pull/49
+# The following lines have been copied from the original implementation.
+# They patch the broken extraction of integers as per the unmerged PR.
+#
+# Upstream code:
+# https://github.com/srossross/rpmfile/pull/49
+#
+# Original copyright:
+#
+# -------------------------------------------------------------------------
+#
+# Copyright (c) 2015 Sean Ross-Ross
+#
+# MIT License
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# -------------------------------------------------------------------------
+#
+# This modified version can be used under either the MIT or the Apache-2.0 license,
+# depending on your preferences.
 def _extract_int32(offset: int, count: int, store: bytes) -> int | tuple[int]:
     import struct
     values = struct.unpack(b"!" + b"I" * count, store[offset: offset + 4 * count])
